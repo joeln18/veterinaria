a1d1aa07bc6de0bc88457ef4edf4f1c27f11557d
joeln18 <joeln18@hotmail.com>
2019-05-14 16:36:24 -0500

    creación del login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,19 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->truncateTables([
-            'rol', 'permiso',
-        ]);
-        $this->call(RolTableSeeder::class);
-        $this->call(PermisoTableSeeder::class);
+    
     }
     
-
-    protected function truncateTables(array $tables){
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        foreach($tables as $table){
-            DB::table($table)->truncate();
-        }
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
-    }
 }
